ff4102a264e9507c0095a619214e50702327c006
tulipbayy <btulepbayeva@smith.edu>
2026-03-28 00:41:59 -0400

    added draw

--- src/App.js
+++ src/App.js
@@ -26,6 +26,8 @@ function Board({ xIsNext, squares, onPlay }) {
   let status;
   if (winner) {
     status = 'Winner: ' + winner;
+  } else if (squares.every(square => square !== null)) {
+  status = "It's a draw!";
   } else {
     status = 'Next player: ' + (xIsNext ? 'X' : 'O');
   }
